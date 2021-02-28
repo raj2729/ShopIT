a03eb5d5e1a0bf2c41069a6c85f4e0d64fb05ee5
Raj <rajsanghavi9@gmail.com>
2021-02-28 11:16:11 +0530

    Added product model and new product route

---
+++ backend/routes/products.js
@@ -0,0 +1,15 @@
+const express=require("express"); 
+const router=express.Router();
+
+const { getProducts , newProduct } = require('../controllers/productController');
+
+router  
+  .route('/products')
+
+  .get(getProducts)
+
+router
+  .route('/pruduct/new')
+
+  .post(newProduct)
+module.exports = router;
